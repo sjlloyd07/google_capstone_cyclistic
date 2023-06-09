a5c72f82cabca5b8aa96aa33dc961293b45fa66a
steve <97957522+sjlloyd07@users.noreply.github.com>
2023-06-09 13:01:07 -0500

    Update capstone_scrape.py

--- capstone_scrape.py
+++ capstone_scrape.py
@@ -15,17 +15,13 @@ from selenium import webdriver # webdriver to navigate to page objects
 url = "https://divvy-tripdata.s3.amazonaws.com/index.html"
 
 # save destination folder
-
 folder = r'C:\Users\steve\OneDrive\Desktop\Google Data Analytics\Capstone\Track 1\zipfiles'
 os.makedirs(folder)
-# visually inspect page and determine download elements
-# desired result from trial and error: ('css selector', 'td a') contains filenames & links
 
-# custom TestBot firefox profile has ghostery and ublockOrigin
-# profile = webdriver.FirefoxProfile(r'C:\Users\steve\AppData\Roaming\Mozilla\Firefox\Profiles\snws8jty.TestBot')
+# visual page source inspection revealed neccessary download elements
+# result: ('css selector', 'td a') contains filenames & links
 
 #navigate to website using custom firefox profile
-#driver = webdriver.Firefox(profile)
 driver = webdriver.Firefox()
 driver.get(url)
 time.sleep(1) #allow page time to load
@@ -38,26 +34,10 @@ for i in range(len(web_elem_lst)):
     # get Response object for each webelement
     res = requests.get(web_elem_lst[i].get_attribute('href'))
     res.raise_for_status
-    # download each Response object content (download items = .zipfiles)
+    # download each Response object content to destination folder(download items = .zipfiles)
     with open(os.path.join(folder, web_elem_lst[i].text), 'wb') as file:
         file.write(res.content)
     
         time.sleep(2) #time to download each file
 
-
-
-
-### not working yet
-
-# local zip file destination folder
-
-# csv_datafiles = r'C:\Users\steve\OneDrive\Desktop\Google Data Analytics\Capstone\Track 1\zipfiles\unzipped'
-# os.makedirs(csv_datafiles)
-
-# # unzip files to destination
-# for zip in os.listdir(folder):
-#     if zipfile.is_zipfile(zip):
-#         if zip[:4] == '2022' or zip[:4] == '2023': 
-#             zipfile.ZipFile(os.path.join(folder, zip)).extractall(csv_datafiles)
-#         continue
 
